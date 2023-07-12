33347fb65cb9e15578f972a93d97061146e3664c
nope0124 <kami-ryo0124@ezweb.ne.jp>
2023-07-13 01:22:54 +0900

    リファクタリング、Pos、Dir

--- main_dfs.cpp
+++ main_dfs.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-// #include <atcoder/all>
 using namespace std;
 typedef long long int ll;
 typedef long double ld;
@@ -7,9 +6,6 @@ typedef pair<ll, ll> pll;
 #define rep(i, N) for(ll i = 0; i < (ll)N; i++)
 #define ALL(v) (v).begin(), (v).end()
 
-ll dx[5] = {-1, 1, 0, 0, 0};
-ll dy[5] = {0, 0, -1, 1, 0};
-string dir = "LRUDN";
 const ll INF = 1LL << 60;
 const ll MOD = 1000000007;
 
@@ -42,65 +38,141 @@ class TimeKeeper {
         }
 };
 
+
+class Dir {
+    public:
+        int dx_;
+        int dy_;
+        char c_;
+        int score_;
+
+        Dir(int dx = 0, int dy = 0, char c = 'N', int score = 0) : dx_(dx), dy_(dy), c_(c), score_(score) { }
+
+        bool operator<(const Dir& other) {
+            return score_ < other.score_;
+        }
+
+        friend string operator+(const string& s, const Dir& d) {
+            return s + string(1, d.c_);
+        }
+
+};
+
+vector<Dir> dir = {Dir(-1, 0, 'L', 0), Dir(+1, 0, 'R', 1), Dir(0, -1, 'U', 2), Dir(0, +1, 'D', 3), Dir(0, 0, 'N', 4)};
+
+class Pos {
+    public:
+        int x_;
+        int y_;
+
+        Pos(int x = 0, int y = 0) : x_(x), y_(y) { }
+
+        bool inRange(int width, int height) {
+            if(this->x_ < 0 || this->x_ >= width || this->y_ < 0 || this->y_ >= height) {
+                return false;
+            }
+            return true;
+        }
+
+        Pos operator+(const Dir& dir) {
+            return Pos(x_ + dir.dx_, y_ + dir.dy_);
+        }
+
+        friend istream& operator>>(istream &is, Pos &p) {
+            return is >> p.y_ >> p.x_;
+        }
+
+        friend ostream& operator<<(ostream &os, const Pos &p) {
+            return os << "Pos: (" << p.x_ << ", " << p.y_ << ')';
+        }
+
+};
+
+class Ans {
+    public:
+        ll score_;
+        string operation_;
+        Ans(ll score = 0, string operation = "") : score_(score), operation_(operation) { }
+
+        friend ostream& operator<<(ostream &os, const Ans &ans) {
+            return os << ans.operation_;
+        }
+
+        bool operator<(const Ans& other) {
+            if(score_ == other.score_) {
+                return operation_.size() < other.operation_.size();
+            }
+            return score_ < other.score_;
+        }
+};
+
 const ll N = 50;
-const ll time_threshold = 1900;
+const ll time_threshold = 100;
 
-ll sx, sy;
-ll old_score = 0;
-string best_ans = "";
-vector<vector<ll>> tile_direction(N, vector<ll>(N));
-vector<vector<ll>> tile(N, vector<ll>(N));
-vector<vector<ll>> point(N, vector<ll>(N));
-vector<vector<bool>> reached(N, vector<bool>(N, false));
+Pos start_pos;
+Ans best_ans;
+ll tile[N][N];
+Dir tile_dir[N][N];
+ll point[N][N];
 
-bool is_range(ll x, ll y) {
-    if(x < 0 || x >= N || y < 0 || y >= N) return false;
-    return true;
-}
+class Board {
+    public:
+        vector<vector<bool>> reached_;
 
+        Board(ll width, ll height) {
+            reached_.resize(height, vector<bool>(width, false));
+        }
 
-bool dfs(ll x, ll y, ll cur_score, string ans, TimeKeeper time_keeper) {
-    if(time_keeper.isTimeOver()) {
-        return true;
-    }
+        void fill(Pos p, bool flag = true) {
+            reached_[p.y_][p.x_] = flag;
+            reached_[p.y_ + tile_dir[p.y_][p.x_].dy_][p.x_ + tile_dir[p.y_][p.x_].dx_] = flag;
+        }
+};
+
+Board board(N, N);
+
+bool dfs(Pos p, Ans cur_ans, TimeKeeper time_keeper) {
+    if(time_keeper.isTimeOver()) return true;
     
-    reached[y][x] = true;
-    reached[y + dy[tile_direction[y][x]]][x + dx[tile_direction[y][x]]] = true;
+    board.fill(p, true);
 
-    vector<pll> cur_dir;
+    vector<Dir> cur_dir;
     rep(i, 4) {
-        ll nx = x + dx[i];
-        ll ny = y + dy[i];
-        if(!is_range(nx, ny)) continue;
-        if(reached[ny][nx]) continue;
-        cur_dir.push_back(pll(-((N/2-nx)*(N/2-nx) + (N/2-ny)*(N/2-ny)), i));
+        Pos np = p + dir[i];
+        if(!np.inRange(N, N)) continue;
+        if(board.reached_[np.y_][np.x_]) continue;
+        Dir nd = dir[i];
+        nd.score_ = (N/2-np.x_)*(N/2-np.x_) + (N/2-np.y_)*(N/2-np.y_);
+        cur_dir.push_back(nd);
     }
 
     if(cur_dir.size() != 0) {
         sort(ALL(cur_dir));
+        reverse(ALL(cur_dir));
         rep(i, cur_dir.size()) {
-            ll nx = x + dx[cur_dir[i].second];
-            ll ny = y + dy[cur_dir[i].second];
-            if(!is_range(nx, ny)) continue;
-            if(reached[ny][nx]) continue;
-            bool time_limit = dfs(nx, ny, cur_score + point[ny][nx], ans + dir[cur_dir[i].second], time_keeper);
+            Pos np = p + cur_dir[i];
+            if(!np.inRange(N, N)) continue;
+            if(board.reached_[np.y_][np.x_]) continue;
+            Ans new_ans = cur_ans;
+            new_ans.score_ += point[np.y_][np.x_];
+            new_ans.operation_ = new_ans.operation_ + cur_dir[i];
+            bool time_limit = dfs(np, new_ans, time_keeper);
             if(time_limit) return true;
         }
     }
 
-    if(old_score < cur_score) {
-        old_score = cur_score;
-        best_ans = ans;
-    }
+    if(best_ans < cur_ans) best_ans = cur_ans;
 
-    reached[y][x] = false;
-    reached[y + dy[tile_direction[y][x]]][x + dx[tile_direction[y][x]]] = false;
+    board.fill(p, false);
+
     return false;
 }
 
 int main() {
     TimeKeeper time_keeper = TimeKeeper(time_threshold);
-    cin >> sy >> sx;
+
+    // input
+    cin >> start_pos;
     rep(i, N) {
         rep(j, N) {
             cin >> tile[i][j];
@@ -114,19 +186,16 @@ int main() {
     rep(i, N) {
         rep(j, N) {
             rep(k, 5) {
-                ll nx = j + dx[k];
-                ll ny = i + dy[k];
-                if(!is_range(nx, ny)) continue;
-                if(tile[i][j] == tile[ny][nx]) {
-                    tile_direction[i][j] = k;
+                Pos np = Pos(j, i) + dir[k];
+                if(!np.inRange(N, N)) continue;
+                if(tile[i][j] == tile[np.y_][np.x_]) {
+                    tile_dir[i][j] = dir[k];
                     break;
                 }
             }
         }
     }
 
-    reached[sy][sx] = true;
-    reached[sy + dy[tile_direction[sy][sx]]][sx + dx[tile_direction[sy][sx]]] = true;
-    dfs(sx, sy, 0, "", time_keeper);
+    dfs(start_pos, Ans(0, ""), time_keeper);
     cout << best_ans << endl;
 }
